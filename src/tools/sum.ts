a26681fa432c044b07d3e413f39d8ffb0fa1aee4
radoslav-marinov-allane <radoslav.marinov.extern@allane.com>
2024-04-23 18:45:57 +0300

    asdas

--- src/tools/sum.ts
+++ src/tools/sum.ts
@@ -1,5 +1,6 @@
 async function main(args: string[]) {
   console.log(`👉 process.env.NODE_ENV = `, process.env.NODE_ENV);
+  console.log(`❌  SECRET  = `, process.env.MY_SECRET);
   console.log(`👉 Runing script: `, args[0]);
   console.log(`👉 With arguments: `, args.slice(2).join(","));
 }
